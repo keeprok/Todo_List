d56a03293843a1c91fd9e3a6177da41dda658eb4
keeprok <140785214+keeprok@users.noreply.github.com>
2025-04-19 20:01:57 +0900

    🎨 : header 추가

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
+export const Header = () => {
+  const router = useRouter();
+  return (
+    <header className="w-full border-b-1 border-slate-200 bg-slate-200">
+      {/* 데스크탑: 살짝 왼쪽정렬 + 아이콘 글씨*/}
+      <div className="hidden lg:flex  w-full pl-[360px]">
+        <button onClick={() => router.push('/')}>
+          <Image src="/img/logo/textLogo.png" alt="logo" width={151} height={40} priority />
+        </button>
+      </div>
+
+      {/* 테블릿 */}
+      <div className="hidden sm:flex lg:hidden px-[16px]">
+        <button onClick={() => router.push('/')}>
+          <Image src="/img/logo/textLogo.png" alt="logo" width={151} height={40} priority />
+        </button>
+      </div>
+
+      {/* 모바일 */}
+      <div className="flex sm:hidden w-full px-[16px] ">
+        <button onClick={() => router.push('/')}>
+          <Image src="/img/logo/noneLogo.png" alt="logo" width={71} height={40} priority />
+        </button>
+      </div>
+    </header>
+  );
+};
