b72688f66f650120a26f54a54017c16e84af2445
keeprok <140785214+keeprok@users.noreply.github.com>
2025-04-19 18:12:22 +0900

    🎨 : todoList 완성

---
+++ src/features/todo/components/main/TodoList.tsx
@@ -0,0 +1,64 @@
+'use client';
+import Image from 'next/image';
+import { TodoItem } from './TodoItem';
+
+export const TodoList = () => {
+  const lddd = [
+    { id: 1, name: '1string', isCompleted: true, onToggle: () => console.log(1) },
+    { id: 2, name: '1string', isCompleted: true, onToggle: () => console.log(2) },
+  ];
+  const ldddd = [
+    { id: 3, name: '1string', isCompleted: true, onToggle: () => console.log(1) },
+    { id: 4, name: '1string', isCompleted: true, onToggle: () => console.log(2) },
+  ];
+  return (
+    <div className="flex flex-col lg:flex-row gap-6 justify-between w-full ">
+      <div className="w-full lg:w-1/2 flex flex-col gap-4">
+        <Image src="/img/todo.png" alt="logo" width={101} height={36} priority />
+        {lddd.length > 0 ? (
+          lddd.map((data) => (
+            <TodoItem
+              key={data.id}
+              id={data.id}
+              name={data.name}
+              isCompleted={data.isCompleted}
+              onToggle={data.onToggle}
+            />
+          ))
+        ) : (
+          <div className={`w-full lg:w-1/2 flex flex-col ${lddd ? 'gap-4 lg:gap-[14px]' : ' lg:gap-16'}`}>
+            <Image src="/img/empty/baseBig" alt="할 일이 없어요" width={240} height={240} priority />
+            <p className=" text-center font-bold text-slate-400 leading-relaxed">
+              할 일이 없어요.
+              <br />
+              TODO 를 새롭게 추가해주세요!
+            </p>
+          </div>
+        )}
+      </div>
+      <div className={`w-full lg:w-1/2 flex flex-col ${ldddd ? 'gap-4 lg:gap-[14px]' : 'gap-0 sm:gap-6 lg:gap-16'}`}>
+        <Image src="/img/done.png" alt="logo" width={97} height={36} priority />
+        {ldddd.length > 0 ? (
+          ldddd.map((data) => (
+            <TodoItem
+              key={data.id}
+              id={data.id}
+              name={data.name}
+              isCompleted={data.isCompleted}
+              onToggle={data.onToggle}
+            />
+          ))
+        ) : (
+          <div className="flex flex-col items-center justify-center py-10 text-slate-400 gap-6">
+            <Image src="/img/empty/smileBig.png" alt="아직 다 한 일이 없어요" width={160} height={160} priority />
+            <p className=" text-center font-bold text-slate-400 leading-relaxed">
+              아직 다 한 일이 없어요.
+              <br />
+              해야 할 일을 체크해보세요!
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
